fbfd8172d8953dc7568916ed84c424dc4861a135
Ricardo Oliveira <rfo20112004@gmail.com>
2024-02-27 15:04:56 +0000

    linting fix to aula_1

--- aula_1/aula_1.r
+++ aula_1/aula_1.r
@@ -1,65 +1,71 @@
+data <- read.csv("DadosFolha1.csv", header = TRUE)
+
 ex_3 <- function() {
-    data <- read.csv("DadosFolha1.csv", header = TRUE, nrows=30)
-    vector <- data[,1]
-    print(paste("The average of the set is: ", mean(vector)))
-    print(paste("The median of the set is: ", median(vector)))
-    print(paste("The std. dev. of the set is: ", sd(vector)))
+  vector <- na.omit(data$Ex3)
+  stripchart(vector, method = "stack")
+  print(paste("The average of the set is: ", mean(vector)))
+  print(paste("The median of the set is: ", median(vector)))
+  print(paste("The std. dev. of the set is: ", sd(vector)))
 }
 
 ex_4 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=35)
-    vector <- data[,2]
-    # nclass.Sturges(vector)  # opcional
-    hist(vector, right=F, main="Perímetro cefálico", col="blue", xlab="Perímetro (cm)", ylab="Frequência")
-    print(paste("The average of the set is: ", mean(vector)))
-    print(paste("The std. dev. of the set is: ", sd(vector)))
+  vector <- na.omit(data$Ex4)
+  # optional - nclass.Sturges(vector)
+  hist(vector, right = FALSE, main = "Perímetro cefálico", col = "blue"
+       , xlab = "Perímetro (cm)", ylab = "Frequência")
+  print(paste("The average of the set is: ", mean(vector)))
+  print(paste("The std. dev. of the set is: ", sd(vector)))
 }
 
 ex_5 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=36)
-    vector <- data[,3]
-    # nclass.Sturges(vector)   # opcional
-    hist(vector, right=F, main="Largura do molar", col="blue", xlab="Largura", ylab="Frequência")
+  vector <- na.omit(data$Ex5)
+  # optional - nclass.Sturges(vector)
+  hist(vector, right = FALSE, main = "Largura do molar", col = "blue"
+       , xlab = "Largura", ylab = "Frequência")
 }
 
 ex_6 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=18)
-    vector <- data[,4]
-    hist(vector, right=F, main="Atividade de enzima", col="blue", xlab="Atividade", ylab="Frequência", breaks=seq(4, 19, by=3))
-    print("The value of the quartiles is: ")
-    print(quantile(vector, , type = 2))
-    print(paste("The value of AIQ is: ", quantile(vector, type=2, 0.75) - quantile(vector, type=2, 0.25)))
+  vector <- na.omit(data$Ex6)
+  hist(vector, right = FALSE, main = "Atividade de enzima", col = "blue"
+       , xlab = "Atividade", ylab = "Frequência", breaks = seq(4, 19, by = 3))
+  print("The value of the quartiles is: ")
+  print(quantile(vector, , type = 2))
+  print(paste("The value of AIQ is: ", quantile(vector, type = 2, 0.75)
+              - quantile(vector, type = 2, 0.25)))
 }
 
 ex_7 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=500)
-    vector <- data[,5]
-    hist(vector, right=F, main="Comprimento de tripanossomas", col="blue", xlab="Comprimento (em micrómetros)", ylab="Frequência", breaks=seq(15, 38, by=1))
-    # hist(vector, right=F, main="Comprimento de tripanossomas", col="blue", xlab="Comprimento (em micrómetros)", ylab="Frequência", breaks=seq(15, 39, by=4))
+  vector <- na.omit(data$Ex7)
+  hist(vector, right = FALSE, main = "Tripanossomas", col = "blue"
+       , xlab = "Comprimento (em micrómetros)", ylab = "Frequência"
+       , breaks = seq(15, 38, by = 1))
+  # hist(vector, right=F, main="Comprimento de tripanossomas", col="blue"
+  #      , xlab="Comprimento (em micrómetros)", ylab="Frequência"
+  #      , breaks=seq(15, 39, by=4))
 }
 
 ex_9 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=20)
-    vector <- data[,6]
-    hist(vector, right=F, main="Idade de diabéticos", col="blue", xlab="Idade (em anos)", ylab="Frequência")
-    print(paste("The average of the set is: ", mean(vector)))
-    print(paste("The std. dev. of the set is: ", sd(vector)))
+  vector <- na.omit(data$Ex9)
+  hist(vector, right = FALSE, main = "Idade de diabéticos", col = "blue"
+       , xlab = "Idade (em anos)", ylab = "Frequência")
+  print(paste("The average of the set is: ", mean(vector)))
+  print(paste("The std. dev. of the set is: ", sd(vector)))
 }
 
 ex_11 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=36)
-    vector <- data[,7]
-    barplot(table(vector), main="Tamanho das ninhadas", xlab="nº de leitões", ylab="Frequência", col="blue")
-    print(paste("The average of the set is: ", mean(vector)))
-    print(paste("The median of the set is: ", median(vector)))
-    print(paste("The std. dev. of the set is: ", sd(vector)))
+  vector <- na.omit(data$Ex11)
+  barplot(table(vector), main = "Tamanho das ninhadas", xlab = "nº de leitões"
+          , ylab = "Frequência", col = "blue")
+  print(paste("The average of the set is: ", mean(vector)))
+  print(paste("The median of the set is: ", median(vector)))
+  print(paste("The std. dev. of the set is: ", sd(vector)))
 }
 
 ex_12 <- function() {
-    data<- read.csv("DadosFolha1.csv", header= TRUE, nrows=15)
-    vector <- data[,8]
-    hist(vector, right=F, main="Altura de plantas", xlab="altura (em cm)", ylab="Frequência", col="blue")
-    print(paste("The average of the set is: ", mean(vector)))
-    print(paste("The std. dev. of the set is: ", sd(vector)))
-    print(paste("The variance of the set is: ", var(vector)))
+  vector <- na.omit(data$Ex12)
+  hist(vector, right = FALSE, main = "Altura de plantas"
+       , xlab = "altura (em cm)", ylab = "Frequência", col = "blue")
+  print(paste("The average of the set is: ", mean(vector)))
+  print(paste("The std. dev. of the set is: ", sd(vector)))
+  print(paste("The variance of the set is: ", var(vector)))
 }
